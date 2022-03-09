57c4bd9d157b68e8ed0a6ac0baad2dd5a202a611
Toba <tobajamezmadamori@gmail.com>
2022-03-09 11:22:44 +0100

    added the auth middleware to get-user route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const { register, login, getUser } = require('../controllers/user')
 const upload = require('../utils/multer')
+const authMiddleware = require('../middleware/auth')
 
 
 router.post('/register', upload.single('avatar'), register)
 router.post('/login', login)
-router.get('/get-user', getUser) // cannot get a particular user/profile if you are not authenticated
+router.get('/get-user', authMiddleware, getUser) // cannot get a particular user/profile if you are not authenticated
 
 module.exports = router;
