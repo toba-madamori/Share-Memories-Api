6d19daa368fafa6bd0b62c14bcda0dddd239a578
Toba <tobajamezmadamori@gmail.com>
2022-03-10 13:33:13 +0100

    added the router setup for the memory controllers

---
+++ routes/memories.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const upload = require('../utils/multer')
+const { 
+    getAllMemories,
+    getAMemory,
+    createMemory,
+    updateAMemory,
+    deleteAMemory, 
+} = require('../controllers/memories')
+
+
+router.get('/get-all-memories', getAllMemories)
+router.get('/get-a-memory/:id', getAMemory)
+router.post('/create-a-memory/:id', upload.single('memory'), createMemory)
+router.patch('/update-a-memory/:id', upload.single('memory'), updateAMemory)
+router.delete('/delete-a-memory/:id', deleteAMemory)
+
+
+module.exports = router;
