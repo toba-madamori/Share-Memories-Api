ff0b50b7e71c9f5596f81908e5c23f0f4f5077c9
Toba <tobajamezmadamori@gmail.com>
2022-03-10 13:44:34 +0100

    modified memories-route file

--- routes/memories.js
+++ routes/memories.js
@@ -11,7 +11,7 @@ const {
 
 router.get('/get-all-memories', getAllMemories)
 router.get('/get-a-memory/:id', getAMemory)
-router.post('/create-a-memory/:id', upload.single('memory'), createMemory)
+router.post('/create-a-memory', upload.single('memory'), createMemory)
 router.patch('/update-a-memory/:id', upload.single('memory'), updateAMemory)
 router.delete('/delete-a-memory/:id', deleteAMemory)
 
