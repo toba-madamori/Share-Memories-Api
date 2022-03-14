7812a9593eceb32bc8abe7e1084211a274d969f4
Toba <tobajamezmadamori@gmail.com>
2022-03-14 10:36:34 +0100

    added pagination for the memory-search functionality

--- controllers/memories.js
+++ controllers/memories.js
@@ -104,9 +104,18 @@ const memorySearch = async(req,res)=>{
         queryObject.tags = { $regex:tags, $options:'i' }
     }
     // searching the memories
-    const result = await Memory.find(queryObject)
-    if(result.length > 0){
-        return res.status(StatusCodes.OK).json({ result, nbhits:result.length })
+    let result = Memory.find(queryObject)
+
+    // pagination and limiting of the data recieved
+    // options are [page:page number, limit:no of products sent back]
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1)*limit
+    result = result.skip(skip).limit(limit)
+
+    const memories = await result
+    if(memories.length > 0){
+        return res.status(StatusCodes.OK).json({ memories, nbhits:memories.length })
     }
     res.status(StatusCodes.OK).json({ msg:'sorry no memory with this title or tag'})
 }
