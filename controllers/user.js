d37d3e6f5c2f6d08521d7e62ac7893b7f3d89788
Toba <tobajamezmadamori@gmail.com>
2022-03-14 10:27:56 +0100

    added pagination for user-general-search functionality

--- controllers/user.js
+++ controllers/user.js
@@ -144,14 +144,23 @@ const deleteUserAccount = async(req,res)=>{
 // search params: username
 // note that after returning the possible usernames, the client is responsible for implementing a specific user 
 // search with those possible usernames
+// pagination query params are page and limit!!!
 const userGeneralSearch = async(req,res)=>{
     const { username } = req.query
     if(username===""){
         throw new BadRequestError('sorry the search bar cannot be empty')
     }
-    const users = await User.find({ name:{ $regex:username, $options:'i' }}).select('-password -cloudinary_id')
-    if(users.length > 0){
-        return res.status(StatusCodes.OK).json({ users, nbhits:users.length })
+    let users = User.find({ name:{ $regex:username, $options:'i' }}).select('-password -cloudinary_id')
+    // pagination and limiting of the data recieved
+    // options are [page:page number, limit:no of products sent back]
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1)*limit
+    users = users.skip(skip).limit(limit)
+
+    const result = await users
+    if(result.length > 0){
+        return res.status(StatusCodes.OK).json({ result, nbhits:result.length })
     }
     res.status(StatusCodes.OK).json({ msg:'sorry there is no user with these username' })
 }
