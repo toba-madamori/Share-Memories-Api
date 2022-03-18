a619d4ce63e0bf635ceff97db4771dcacedbbf6b
Toba <tobajamezmadamori@gmail.com>
2022-03-18 15:22:49 +0100

    added the dislike-a-memory functionality

--- controllers/reactions.js
+++ controllers/reactions.js
@@ -30,7 +30,29 @@ const LikeAMemory = async(req,res)=>{
 }
 
 const DislikeAMemory = async(req,res)=>{
-    res.status(StatusCodes.OK).json({ msg:'a user just disliked a memory' })
+    const { id:memoryID } = req.params
+    const { userID } = req.user
+
+    // checking if the user has disliked the memory before
+    const prevDislike = await Dislikes.findOneAndDelete({ userid:userID, memoryid:memoryID })
+    if(prevDislike){
+        // removing the dislike from the memory
+        const memory1 = await Memory.findByIdAndUpdate({ _id:memoryID }, {$inc:{ dislikes:-1 }}, {runValidators:true})
+        return res.status(StatusCodes.OK).json({ msg:'removed the dislike' })
+    }
+    // checking if the user has liked the memory before
+    const prevLike = await Likes.findOneAndDelete({ userid:userID, memoryid:memoryID })
+    if(prevLike){
+        // removing the like from the memory
+        const memory2 = await Memory.findByIdAndUpdate({ _id:memoryID }, {$inc:{ likes:-1 }}, {runValidators:true})
+        return res.status(StatusCodes.OK).json({ msg:'removed the like' })
+    }
+    // creating and adding the dislike to the memory
+    const newDislike = await Dislikes.create({ userid:userID, memoryid:memoryID })
+    if(newDislike){
+        const memory3 = await Memory.findByIdAndUpdate({ _id:memoryID }, {$inc:{ dislikes:1 }}, {new:true, runValidators:true })
+        return res.status(StatusCodes.OK).json({ memory3, newDislike })
+    }
 }
 
 module.exports = {
