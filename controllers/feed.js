4f7360d605fce2e60e3df865a602d7e004a05fde
Toba <tobajamezmadamori@gmail.com>
2022-03-24 16:37:37 +0100

    added some functionality to the user-feed generator

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,8 +3,16 @@ const User = require('../models/user')
 const Likes = require('../models/likes')
 const Memory = require('../models/memories')
 
+// this 3 different likes will always be used to generate a feed for a user that has not liked any previous memory
 const initialLikesFeed = async(req,res)=>{
-    res.status(StatusCodes.OK).json({ msg:'Initial likes feed' })
+    const { like1, like2, like3 } = req.body
+    const arr = [like1, like2, like3]
+    const query = arr.join()
+
+    // searching for memories with the following like keywords
+   const memories = await Memory.find({ tag :{ $regex:query, $options:'i' }})
+
+    res.status(StatusCodes.OK).json({ memories })
 }
 
 const finalLikesFeed = async(req,res)=>{
